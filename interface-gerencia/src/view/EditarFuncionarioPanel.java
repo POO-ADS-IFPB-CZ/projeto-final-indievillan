69ff8738781fd4eb9e8ee888c520c08a713b6256
Pedro Holanda <86068661+pedroandrade932@users.noreply.github.com>
2025-08-26 18:03:25 -0300

    Adicionando o visualizar e editar

---
+++ interface-gerencia/src/view/EditarFuncionarioPanel.java
@@ -0,0 +1,22 @@
+package view;
+import controller.AppController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
+public class EditarFuncionarioPanel extends JPanel {
+    private AppController controller;
+
+    public EditarFuncionarioPanel(AppController controller){
+        this.controller = controller;
+        setLayout(new GridLayout(10, 2, 10, 10));
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        JTextField textField = new JTextField("TABELA DE FUNCIONARIO", 20);
+        JButton btnVoltar = new JButton("Voltar");
+
+        add(btnVoltar);
+    }
+}
