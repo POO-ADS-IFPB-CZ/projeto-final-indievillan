dbbc2936b75a844ec6b1bf04bbc517e813521d03
Pedro Holanda <86068661+pedroandrade932@users.noreply.github.com>
2025-08-26 16:49:42 -0300

    adicionando o menu principal

---
+++ interface-gerencia/src/view/MenuPrincipalPanel.java
@@ -0,0 +1,36 @@
+package view;
+
+import controller.AppController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
+public class MenuPrincipalPanel extends JPanel {
+
+    private AppController controller;
+
+    public MenuPrincipalPanel(AppController controller) {
+        this.controller = controller;
+        setLayout(new GridLayout(10, 2, 10, 10));
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        JButton btnGestaoFuncionarios = new JButton("Gestão de Funcionários");
+        JButton btnParceiros = new JButton("Parceiros");
+        JButton btnGestaoProjetos = new JButton("Gestão de Projetos");
+        JButton btnAdministrativo = new JButton("Administrativo");
+
+        add(btnGestaoFuncionarios);
+        add(btnParceiros);
+        add(btnGestaoProjetos);
+        add(btnAdministrativo);
+
+        btnGestaoFuncionarios.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.showGestaoFuncionarioMenu();
+            }
+        });
+    }
+}
